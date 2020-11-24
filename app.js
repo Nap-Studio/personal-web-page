b0c6f91baa287026b77075ecafe3e02e53f7c4bd
Mauro Napsuciale <mnap95@gmail.com>
2020-11-24 17:08:08 -0600

    Added work section functionality

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
 
-function changeContent(newContent){
+function changeContent(newContent) {
     let activeContent = document.querySelector('.active');
     activeContent.classList.remove('active');
     activeContent.classList.add('hidden');
@@ -14,4 +14,24 @@ function changeContent(newContent){
     let newContainer = document.querySelector(`.${newContent}`);
     newContainer.classList.remove('hidden');
     newContainer.classList.add('active');
+}
+
+function changeDetails(index) {
+    let infoText = document.querySelector('.info-container p');
+    switch (index) {
+        case 1:
+            infoText.innerHTML = "Amazonia | 2018"
+            break;
+        case 2:
+            infoText.innerHTML = "Road To Aztlan | 2019"
+            break;
+        default:
+            infoText.innerHTML = "I Want To Be a Beer | 2017"
+            break;
+    }
+}
+
+function revertDetails() {
+    let infoText = document.querySelector('.info-container p');
+    infoText.innerHTML = "My Work"
 }
