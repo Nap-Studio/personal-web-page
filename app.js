9bd32320d248f48512d93c8e2b1d3436daf7edfe
Mauro Napsuciale <mnap95@gmail.com>
2020-11-28 21:50:58 -0600

    Fixed typos in css links

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 particlesJS.load('particles-js', 'assets/particles.json', function () {
-    console.log('callback - particles.js config loaded');
 });
-
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
 
 function changeContent(newContent) {
     let activeContent = document.querySelector('.active');
